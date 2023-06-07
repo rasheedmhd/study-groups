d115fa937863ac398f0288997fa122d38253366b
Virux <starletgh@gmail.com>
2023-06-07 21:34:24 +0000

    7 Jun

--- rust/structs/carbohydrate/src/monosaccharide.rs
+++ rust/structs/carbohydrate/src/monosaccharide.rs
@@ -3,7 +3,68 @@ pub enum Class {
     KETONE,
 }
 
-pub struct CarbohydrateStructure {
-    pub carbon: u8,
-    pub class: Class,
+pub enum Version {
+    D,
+    L,
+}
+
+enum FischerProj {
+    H,
+    OH,
+}
+
+
+struct CarbohydrateStructure {
+    carbon: u8,
+    class: Class,
+    version: Version,
+    structure: Vec<FischerProj>,
+}
+
+impl CarbohydrateStructure {
+    fn new(
+        carbon: u8,
+        class: Class,
+        version: Version,
+    )  -> Self {
+        // TODO:
+        // determine Fischer Projection
+    }
+}
+
+// 0 = H
+// 1 = OH
+let version = vec![0, 1];
+
+
+struct D_glucose = {
+    arrangement: [H,HO,H,H];
+}
+
+impl D_glucose {
+    fn new(arrangement)
+}
+
+
+
+
+
+struct arrrangements {
+    left: str,
+    right: str,
+}
+
+// edge
+
+struct glucose {
+    arrangements: [arrangements; 4]
+}
+
+D_<whatever> = {
+        arrangements: [
+            { left: "H", right: "OH"},
+            { left: "H", right: "OH"},
+            { left: "H", right: "OH"},
+            { left: "H", right: "OH"},
+        ]
 }
