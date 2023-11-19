341e98241ac0bcc28f37609a61f19508acccf2e9
Virux <starletgh@gmail.com>
2023-11-19 23:52:45 +0000

    dummy file writer

--- rust/winston/src/main.rs
+++ rust/winston/src/main.rs
@@ -1,7 +1,15 @@
 use std::env;
+use std::fs;
+
+// fn writeToFile(data: String) -> Result<(), Error> {
+//     fs::write("logged.txt", data)
+// }
 
 fn main() {
     let text: Vec<String> = env::args().collect();
     let toLog = &text[1..];
-    println!("Hello, world! {:#?}", toLog);
+    println!("{:?}", toLog);
+
+    // writeToFile(toLog);
+    fs::write("logged.txt", b"hello from sherif");
 }
