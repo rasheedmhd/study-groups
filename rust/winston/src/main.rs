c309eb6d5153b1c19e3bcba760d0d2e041cfbb6d
Virux <starletgh@gmail.com>
2023-11-22 18:59:21 +0000

    winston first demo day

--- rust/winston/src/main.rs
+++ rust/winston/src/main.rs
@@ -1,15 +1,39 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::process;
 
-// fn writeToFile(data: String) -> Result<(), Error> {
-//     fs::write("logged.txt", data)
-// }
+#[derive(Debug)]
+struct Data {
+    data: String,
+}
+
+impl Data {
+    fn validate(args: &[String]) -> Result<Data, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough data to log")
+        }
+        let to_log = args[1].clone();
+        Ok( Data { data: to_log } )
+    }
+}
+
 
 fn main() {
+
     let text: Vec<String> = env::args().collect();
-    let toLog = &text[1..];
+
+    let data = Data::validate(&text).unwrap_or_else(|err| {
+        eprintln!("Problem validating args: {err}");
+        process::exit(1);
+    });
+
+    let toLog = &data;
     println!("{:?}", toLog);
 
+    let mut logs_file = File::create("logs.txt").expect("file creation failed");
+
     // writeToFile(toLog);
-    fs::write("logs.txt", b"hello from black sherif");
+    //    fs::write("logs.txt", b"hello from black sherif");
+    logs_file.write(toLog.data.as_bytes()).expect("write failed");
 }
--- rust/winston/target/debug/winston.d
+++
@@ -1,1 +0,0 @@
-/Users/Starlet/Dev/study-groups/rust/winston/target/debug/winston: /Users/Starlet/Dev/study-groups/rust/winston/src/main.rs
