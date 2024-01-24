7982dadd2c68b71d439084fa67b1f023136921f8
rasheedmhd <starletgh@gmail.com>
2024-01-24 20:23:51 +0000

    NonNull

--- vec/main.rs
+++ vec/main.rs
@@ -2,11 +2,19 @@
 // size of the data stored on the heap
 // capacity ( number of items to be stored on the heap )
 
+// using the NonNull wrapper over *mut T for have covariance
+// and of course non nullable ptr
+use std::ptr::NonNull;
+use std::marker::PhantomData;
+
 pub struct Vec<T> {
     ptr: *mut T,
     len: usize,
     cap: usize,
 }
+
+unsafe impl<T: Send> Send for Vec<T> {}
+unsafe impl<T: Sync> Sync for Vec<T> {}
 
 fn main() {
     //
