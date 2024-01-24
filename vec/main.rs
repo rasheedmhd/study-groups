2fdfb09267cc1f83324051e34683c7698b2f4741
rasheedmhd <starletgh@gmail.com>
2024-01-24 21:01:13 +0000

    Vec::new()

--- vec/main.rs
+++ vec/main.rs
@@ -7,14 +7,35 @@
 use std::ptr::NonNull;
 use std::marker::PhantomData;
 
+use std::mem;
+
 pub struct Vec<T> {
-    ptr: *mut T,
+    ptr: NonNull<T>,
     len: usize,
     cap: usize,
 }
 
 unsafe impl<T: Send> Send for Vec<T> {}
 unsafe impl<T: Sync> Sync for Vec<T> {}
+
+
+// Creating an empty Vec::new() doesn't allocate data in heap
+// Our ptr can't be null obviously 
+// We put some garbage in there.
+// cap = 0, serves as a guard that we aren't allocating anything in mem
+impl<T> Vec<T> {
+    pub fn new() -> Self {
+        assert!(
+            mem::align_of::<T>() != 0, 
+            "Not handling ZST yet!"
+        );
+        Vec {
+            ptr: NonNull::dangling(),
+            len: 0,
+            cap: 0,
+        }
+    } 
+}
 
 fn main() {
     //
