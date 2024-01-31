7de467d69bc6b5eab37f1cd541399b7c7d99c00f
rasheedmhd <starletgh@gmail.com>
2024-01-31 09:47:23 +0000

    vec + grow

--- vec/main.rs
+++ vec/main.rs
@@ -1,12 +1,12 @@
 // pointer to where the data is stored in the heap
 // size of the data stored on the heap
-// capacity ( number of items to be stored on the heap )
+// capacity ( number of items stored on the heap )
 
-// using the NonNull wrapper over *mut T for have covariance
+// using the NonNull wrapper over *mut T for covariance
 // and of course non nullable ptr
 use std::ptr::NonNull;
 use std::marker::PhantomData;
-use std::alloc::{self, Layout}
+use std::alloc::{self, Layout};
 
 use std::mem;
 
@@ -38,13 +38,13 @@ impl<T> Vec<T> {
     }
 
     fn grow(&mut self) {
-        let (new_cap, new_length) if cap.len == 0 {
+        let (new_cap, new_layout) = if self.cap == 0 {
             (1, Layout::array::<T>(1).unwrap())
         } else {
             let new_cap = 2 * self.cap;
             let new_layout = Layout::array::<T>(new_cap).unwrap();
             (new_cap, new_layout)
-        }
+        };
 
         assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
 
@@ -52,7 +52,20 @@ impl<T> Vec<T> {
             unsafe {
                 alloc::alloc(new_layout)
             }
-        }
+        } else {
+            let old_layout = Layout::array::<T>(self.cap).unwrap();
+            let old_ptr = self.ptr.as_ptr() as *mut u8;
+            unsafe {
+                alloc::realloc(old_ptr, old_layout, new_layout.size())
+            }
+        };
+        
+        // If allocation fails new_ptr will be null, so we abort
+        self.ptr = match NonNull::new(new_ptr as *mut T) {
+            Some(p) => p, 
+            None => alloc::handle_alloc_error(new_layout),
+        };
+        self.cap = new_cap
     } 
 }
 
